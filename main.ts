26c9aa05bccf48d4c68562ff3b9a7afd801784e4
Majid AlQasimi <93176523+MajidQue@users.noreply.github.com>
2021-11-01 08:59:59 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+for (let index = 0; index < 4; index++) {
+    basic.showString("HELLO")
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showArrow(ArrowNames.North)
+    }
+}
